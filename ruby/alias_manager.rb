0d772682dba87557d4f95b9d47862f3bf1f1a8d5
Dimesky <matthew.dimesky@gmail.com>
2016-09-20 13:31:23 -0700

    add additonal logic for manipulating string

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -8,11 +8,34 @@
 
 def request_name
 	puts "Please enter your name as long as you AREN't a spy ;) : "
-	spy_name = gets.chomp
+	spy_name = gets.chomp.downcase   #returning user input
 end
 
 def swap_names(name)
-	name.split{' '}.reverse!
+	new_name = name.split(' ').reverse
+	last_name_length = new_name[1].length
+	name_info = [new_name, last_name_length] #returning reverse of first and last name and an the length of the first name
 end
 
-swap_names(request_name)
+def next_letter(name)
+	new_name = []
+	novowels = 'bcdfghjklmnpqrstvwxyz'
+	vowels = 'aeiou'
+	name[0] = name[0].join.split('')
+	name[0].each do |char|
+		if vowels.include?(char)
+			new_name.push(vowels[vowels.index(char) + 1])
+		else
+			new_name.push(novowels[novowels.index(char) + 1])
+		end
+	end
+	new_name.insert(name[1], " ")
+	return new_name
+end
+
+
+p next_letter(swap_names(request_name))
+
+
+
+#push a space value into the array after the length of the first name (or last cant remember)
