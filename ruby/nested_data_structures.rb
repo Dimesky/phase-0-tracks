0349f2fc4219564a7e114dc3ceade381c104784d
Dimesky <matthew.dimesky@gmail.com>
2016-09-20 10:38:06 -0700

    add a nested data structure model of a gym

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,45 @@
+gym = {
+	gym_name: "Gym of Gyms"
+	monthly_membership: 29.99
+	cardio_room: {
+		machines: [
+			"Upright Bike",
+			"Recumbant Bike",
+			"Rowing Machine",
+			"Upper-Body Ergometer",
+			"Treadmill",
+			"Stairmaster",
+			"Elliptical"
+		]
+		time_limit: 30
+		total_machine_count: 30
+	}
+	weight_room: {
+		benches: {
+			flat_bench: 5
+			incline_bench: 2
+			decline_bench: 1
+			squat_rack: 10
+		}
+		people: [
+			"Grunty guy",
+			"Stinky guy",
+			"Makeup girl",
+			"Perfume lady",
+			"Only talks guy"
+		]
+		tvs: 8
+
+	}
+	locker_room: {
+		showers: true
+		towels: false
+		stalls: 3
+	}
+	pool_area: {
+		hot_tub: true
+		steam_room: true
+		sauna: true
+		pool: false
+	}
+}
