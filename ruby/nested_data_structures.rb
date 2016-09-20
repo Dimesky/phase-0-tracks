21045716d3285d2d3e6f86c55e4a59f2304d40d5
Dimesky <matthew.dimesky@gmail.com>
2016-09-20 11:20:57 -0700

    add code to print pieces of nested data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,6 +1,7 @@
 gym = {
-	gym_name: "Gym of Gyms"
-	monthly_membership: 29.99
+	gym_name: "Gym of Gyms",
+	monthly_membership: 29.99,
+	year_discount: 0.12,
 	cardio_room: {
 		machines: [
 			"Upright Bike",
@@ -10,36 +11,54 @@ gym = {
 			"Treadmill",
 			"Stairmaster",
 			"Elliptical"
-		]
-		time_limit: 30
+		],
+		time_limit: 30,
 		total_machine_count: 30
-	}
+	},
 	weight_room: {
 		benches: {
-			flat_bench: 5
-			incline_bench: 2
-			decline_bench: 1
+			flat_bench: 5,
+			incline_bench: 2,
+			decline_bench: 1,
 			squat_rack: 10
-		}
+		},
 		people: [
 			"Grunty guy",
 			"Stinky guy",
 			"Makeup girl",
 			"Perfume lady",
 			"Only talks guy"
-		]
+		
+		],
 		tvs: 8
 
-	}
+	},
 	locker_room: {
-		showers: true
-		towels: false
+		showers: true,
+		towels: false,
 		stalls: 3
-	}
+	},
 	pool_area: {
-		hot_tub: true
-		steam_room: true
-		sauna: true
+		hot_tub: true,
+		steam_room: true,
+		sauna: true,
 		pool: false
 	}
 }
+
+p gym[:gym_name]
+p gym[:gym_name].swapcase!
+p gym[:gym_name]
+p gym[:monthly_membership] * 12 * gym[:year_discount]
+
+gym[:weight_room][:benches].each {|bench, number| puts "#{bench}: There are #{number}"}
+
+gym[:weight_room][:tvs] = 7
+p gym[:weight_room][:tvs]
+
+gym[:pool_area].delete_if {|key, value| value == false}
+p gym[:pool_area]
+
+gym[:weight_room][:people].push "Pushy trainer girl"
+
+gym.each {|key, value| puts "#{key}: #{value}\n__________\n"}
