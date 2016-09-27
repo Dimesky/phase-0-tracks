4b68914a4a07c6446a9e35d70c8b4c46ff7c4fa3
Dimesky <matthew.dimesky@gmail.com>
2016-09-27 11:08:50 -0700

    add loop to instantiate new santas with gender and ethnicity

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -8,13 +8,28 @@ class Santa
 		puts "That was a good #{cookie_type} cookie!"
 	end
 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 
 end
 
-santa = Santa.new
+# santa = Santa.new
 
-santa.speak
-santa.eat_milk_and_cookies("chocolate chip")
+# santa.speak
+# santa.eat_milk_and_cookies("chocolate chip")
+
+arr_of_santa_objects = []
+genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+ethnicities = ["black", "Latino", "white", "Japanese", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+index = 0
+genders.each do |gender|
+	arr_of_santa_objects << Santa.new(gender, ethnicities[index])
+	index += 1
+end
+
+p arr_of_santa_objects
