63886c233a8ebcf7ea883c16b19b12af34c7c868
Dimesky <matthew.dimesky@gmail.com>
2016-09-26 22:41:06 -0700

    add puppy class with characteristics & behavior, and instantiate multiple instances of puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,25 @@
+# SPECIES ------------------------
+# Puppyupicus Cutenomicus
+
+# CHARACTERISTICS ----------------
+# Fur Length: short
+# Friendly: yes
+# Playful: yes
+# Name: varies
+# Color: varies
+# Paws: 4
+# Tail: 1
+
+# BEHAVIOR -----------------------
+# Run
+# Play
+# Poop
+# Nip
+
+class Puppy
+end
+
+morkiepoo = Puppy.new
+german_shepard = Puppy.new
+blue_nose_pitbull = Puppy.new
+
