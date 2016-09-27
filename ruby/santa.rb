e34e0c1a734178a4296a63e7415e58f1a5a7dcdf
Dimesky <matthew.dimesky@gmail.com>
2016-09-27 15:03:19 -0700

    refactor and build many santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -47,19 +47,37 @@ end
 # santa.speak
 # santa.eat_milk_and_cookies("chocolate chip")
 
-arr_of_santa_objects = []
-genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-ethnicities = ["black", "Latino", "white", "Japanese", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-index = 0
-genders.each do |gender|
-	arr_of_santa_objects << Santa.new(gender, ethnicities[index])
-	index += 1
+# arr_of_santa_objects = []
+# genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# ethnicities = ["black", "Latino", "white", "Japanese", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# index = 0
+# genders.each do |gender|
+# 	arr_of_santa_objects << Santa.new(gender, ethnicities[index])
+# 	index += 1
+# end
+
+# arr_of_santa_objects[0].celebrate_birthday
+# arr_of_santa_objects[0].get_mad_at("Rudolph")
+# arr_of_santa_objects[0].gender = "gender neutral"
+# p arr_of_santa_objects[0].age
+# p arr_of_santa_objects[0].ethnicity
+
+# p arr_of_santa_objects[0]
+
+genders = ["agender", "female", "bigender", "male", "gender fluid", "N/A", "gender neutral"]
+ethnicities = ["black", "Latino", "white", "Japanese", "prefer not to say", "Mystical Creature (unicorn)", "N/A", "German", 
+	"French", "Italian","Polish", "Irish", "English", "Armenian", "Iranian", "Jewish", "asian", "mongolian", "Russian"]
+
+
+num_of_santas = 500
+until num_of_santas == 0
+	new_santa = Santa.new(genders.shuffle![0], ethnicities.shuffle![0])
+	birthdays = Random.new
+	birthdays.rand(140).times {|celebrate| new_santa.celebrate_birthday}
+	p new_santa.ethnicity
+	p new_santa.age
+	num_of_santas -= 1
 end
 
-arr_of_santa_objects[0].celebrate_birthday
-arr_of_santa_objects[0].get_mad_at("Rudolph")
-arr_of_santa_objects[0].gender = "gender neutral"
-p arr_of_santa_objects[0].age
-p arr_of_santa_objects[0].ethnicity
 
-p arr_of_santa_objects[0]
+
