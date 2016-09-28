f1a0d040d864d568c4c9fb2ac89eb6c796e1c271
Dimesky <matthew.dimesky@gmail.com>
2016-09-27 17:25:16 -0700

    include module in classes & call instance methods on an instance by way of the module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,34 @@
 module Shout
 	# We'll put some methods here soon, but this code is fine for now!
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
+  def yell_happily(words)
   	words + "!!!" + " :)"
   end
 
 end
 
-p Shout.yell_angrily("AHHHH, I HATE RUBY")
-p Shout.yell_happily("AHHHH, I LOVE RUBY")
+# p Shout.yell_angrily("AHHHH, I HATE EVERYONE")
+# p Shout.yell_happily("AHHHH, I LOVE EVERYONE")
+
+class Weightlifter
+
+	include Shout
+
+end
+
+class Football_coach
+
+	include Shout
+
+end
+
+weightlifter = Weightlifter.new
+p weightlifter.yell_angrily("RAWRARGHHH")
+p weightlifter.yell_happily("RAWRARGHHH")
+
+football_coach = Football_coach.new
+p football_coach.yell_angrily("HEY GET YOUR HEADS OUT OF YOUR BUTTS")
+p football_coach.yell_happily("THATAWAY TEAM")
